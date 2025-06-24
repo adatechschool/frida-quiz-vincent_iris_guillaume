46102d609035baa8bd175cea7dbe1053cd230b02
VincentComier <V.Comier@hotmail.fr>
2025-06-24 15:34:34 +0200

    pull

--- game.js
+++ game.js
@@ -1,60 +1,10 @@
 import Quiz from './questions.js';
 
-const question = document.getElementById("questions");
-const suivant = document.getElementById("submit");
-const reponse = document.getElementById("reponse")
+console.log(Quiz);
 
-let index = 0;
-let score = 0;
+const xxx = document.getElementById("questions")
+console.log(xxx)
 
-//  question.innerText = Quiz[0].question;
-// response1.innerText = Quiz[0].reponses[0].reponse;
-
-
-function afficheQuestion(indice) {
-    question.innerText = Quiz[indice].question;
-    console.log(indice);
-    
+function afficheReponse(questionNumber) {
+    console
 }
-afficheQuestion(index);
-
-
-
-<<<<<<< HEAD
-function afficheReponses(params) {
-    // for (let item of reponse)
-        const reponse = Quiz.reponses.reponse
-    console.log(reponse)
-}
-=======
-function afficheReponses(numeroQuestion) {
-const reponse = Quiz[numeroQuestion].reponses
-console.log(reponse)
-}
-
-afficheReponses(index)
-
-
-
-
-// for (let x of cars) {
-//   text += x;
-// }
-
-
-
-
-
-
-
-
-// function afficheReponses(index) {
-
-    
-
-// console.log(Quiz[0].reponses);
-   
-    
-// }
-// afficheReponses();
->>>>>>> 47b63fb9954243741989da143fd603ea7af8cd4e

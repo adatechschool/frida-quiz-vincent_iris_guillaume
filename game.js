3326d1d256cf4dff6703ca172807a0530376214b
AquaPulco <iris.yolal@gmail.com>
2025-06-24 14:30:05 +0200

    fonction affiche question_/reponses

--- game.js
+++ game.js
@@ -1,3 +1,46 @@
 import Quiz from './questions.js';
 
-console.log(Quiz);
+
+const question = document.getElementById("questions");
+const suivant = document.getElementById("submit");
+
+let index = 0;
+let score = 0;
+
+//  question.innerText = Quiz[0].question;
+//  response1.innerText = Quiz[0].reponses[0].reponse;
+
+
+function afficheQuestion(indice) {
+    question.innerText = Quiz[indice].question;
+    console.log(indice);
+    
+}
+afficheQuestion(index);
+
+
+
+function afficheReponses(params) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// function afficheReponses(index) {
+
+    
+
+// console.log(Quiz[0].reponses);
+   
+    
+// }
+// afficheReponses();
--- questions.js
+++ questions.js
@@ -1,40 +1,43 @@
+
+
 const Quiz = [
     {
         question: "Lequel de ces oiseaux est capable de dormir en volant ?",
-        answers: [
-            { answer: "Le moineau", isCorrect: false },
-            { answer: "Le canari", isCorrect: false },
-            { answer: "Le martinet", isCorrect: true },
-            { answer: "Le corbeau", isCorrect: false }
+        reponses: [
+            { reponse: "Le moineau", isCorrect: false },
+            { reponse: "Le canari", isCorrect: false },
+            { reponse: "Le martinet", isCorrect: true },
+            { reponse: "Le corbeau", isCorrect: false }
         ]
     },
     {
         question: "Quel oiseau a une danse de séduction qui ressemble à un moonwalk version forêt tropicale ?",
-        answers: [
-            { answer: "Le héron Jackson", isCorrect: false },
-            { answer: "Le colibri disco", isCorrect: false },
-            { answer: "Le paradisier superbe", isCorrect: true },
-            { answer: "Le flamant rose funky", isCorrect: false }
+        reponses: [
+            { reponse: "Le héron Jackson", isCorrect: false },
+            { reponse: "Le colibri disco", isCorrect: false },
+            { reponse: "Le paradisier superbe", isCorrect: true },
+            { reponse: "Le flamant rose funky", isCorrect: false }
         ]
     },
     {
         question: "Quel oiseau construit des “maisons de l'amour” super décorées pour impressionner sa dulcinée ?",
-        answers: [
-            { answer: "Le pigeon romantique", isCorrect: false },
-            { answer: "Le corbeau styliste", isCorrect: false },
-            { answer: "Le jardinier satin", isCorrect: true },
-            { answer: "Le perroquet architecte", isCorrect: false }
+        reponses: [
+            { reponse: "Le pigeon romantique", isCorrect: false },
+            { reponse: "Le corbeau styliste", isCorrect: false },
+            { reponse: "Le jardinier satin", isCorrect: true },
+            { reponse: "Le perroquet architecte", isCorrect: false }
         ]
     },
     {
         question: "Lequel de ces oiseaux peut dormir debout... sur une seule patte ?",
-        answers: [
-            { answer: "Le canari yogi", isCorrect: false },
-            { answer: "La Perruche équilibriste", isCorrect: false },
-            { answer: "Le flamant rose", isCorrect: true },
-            { answer: "Le pingouin ninja", isCorrect: false }
+        reponses: [
+            { reponse: "Le canari yogi", isCorrect: false },
+            { reponse: "La Perruche équilibriste", isCorrect: false },
+            { reponse: "Le flamant rose", isCorrect: true },
+            { reponse: "Le pingouin ninja", isCorrect: false }
         ]
     }
 ];
+
 
 export default Quiz;

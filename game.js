951a3dbed4a4a7d26c350adbd47e515e953c7f6d
VincentComier <V.Comier@hotmail.fr>
2025-06-24 15:56:27 +0200

    pull

---
+++ game.js
@@ -0,0 +1,30 @@
+import Quiz from './questions.js';
+
+
+const question = document.getElementById("questions");
+const suivant = document.getElementById("submit");
+const reponse = document.getElementById("reponse")
+
+let index = 0;
+let score = 0;
+
+//  question.innerText = Quiz[0].question;
+// response1.innerText = Quiz[0].reponses[0].reponse;
+
+
+function afficheQuestion(indice) {
+    question.innerText = Quiz[indice].question;
+    console.log(indice);
+    
+}
+afficheQuestion(index);
+
+
+
+function afficheReponses(numeroQuestion) {
+    const reponse = Quiz[numeroQuestion].reponses
+    for (let item of reponse)
+console.log(item.reponse)
+}
+
+afficheReponses(index)
--- questions.js
+++ questions.js
@@ -1,4 +1,3 @@
-import { createElement } from "react";
 
 const Quiz = [
     {

c051d31007b8fb2e920e663966faf340304ca757
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-06-24 10:18:36 +0200

    Ajout Questions JS

---
+++ questions.js
@@ -0,0 +1,101 @@
+const Quiz = {
+    question01 : "Lequel de ces oiseaux est capable de dormir en volant ? ",
+    answers : [
+        { answer01:"Le moineau",
+        isCorrect: false,
+       
+        },
+
+        {
+        answer02:"Le canari",
+        isCorrect: false,
+        
+        },
+        
+        {
+        answer03:"Le martinet",
+        isCorrect: true,
+        },
+        
+        {
+        answere04:"Le corbeau",
+        isCorrect: false,
+    }
+        ],
+
+question02 : "Quel oiseau a une danse de séduction qui ressemble à un moonwalk version forêt tropicale ? ",
+    answers : [
+        { answer01:"Le héron Jackson",
+        isCorrect: false,
+       
+        },
+
+        {
+        answer02:"Le colibri disco",
+        isCorrect: false,
+        
+        },
+        
+        {
+        answer03:"Le paradisier superbe",
+        isCorrect: true,
+        },
+        
+        {
+        answere04:"Le flamant rose funky",
+        isCorrect: false,
+    }
+        ],
+
+
+        question03 : " Quel oiseau construit des “maisons de l'amour” super décorées pour impressionner sa dulcinée ? ",
+    answers : [
+        { answer01:"Le pigeon romantique",
+        isCorrect: false,
+       
+        },
+
+        {
+        answer02:"Le corbeau styliste",
+        isCorrect: false,
+        
+        },
+        
+        {
+        answer03:"Le jardinier satin",
+        isCorrect: true,
+        },
+        
+        {
+        answere04:"Le perroquet architecte",
+        isCorrect: false,
+    }
+        ],
+
+
+
+question04 : " Lequel de ces oiseaux peut dormir debout... sur une seule patte ? ",
+    answers : [
+        { answer01:"Le canari yogi",
+        isCorrect: false,
+       
+        },
+
+        {
+        answer02:"La Perruche équilibriste",
+        isCorrect: false,
+        
+        },
+        
+        {
+        answer03:"Le flamant rose",
+        isCorrect: true,
+        },
+        
+        {
+        answere04:"Le pingouin ninja",
+        isCorrect: false,
+    }
+        ]
+
+}

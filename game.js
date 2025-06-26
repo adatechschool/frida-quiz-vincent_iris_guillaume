a479f136cfbb181b61a3fc829c3d2557b90c74a0
AquaPulco <iris.yolal@gmail.com>
2025-06-26 10:09:33 +0200

    pull score

--- game.js
+++ game.js
@@ -4,10 +4,10 @@ import Quiz from './questions.js';
 const question = document.getElementById("questions");
 const suivant = document.getElementById("submit");
 const reponse = document.getElementById("reponse");
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
+let btn1 = document.getElementById("btn1"); //syntaxe a ameliorer plus tard peut etre car redondant
+let btn2 = document.getElementById("btn2"); //syntaxe a ameliorer plus tard peut etre
+let btn3 = document.getElementById("btn3"); //syntaxe a ameliorer plus tard peut etre
+let btn4 = document.getElementById("btn4"); //syntaxe a ameliorer plus tard peut etre
 
 let tabBoutons = [btn1, btn2, btn3, btn4];
 let correctAnswer;
@@ -15,7 +15,7 @@ let correctAnswer;
 let index = 0;
 let score = 0;
 
-suivant.innerText ="suivant"
+suivant.innerText ="suivant";
 
 //exemple   question.innerText = Quiz[0].question;
 //exemple   response1.innerText = Quiz[0].reponses[0].reponse;
@@ -30,7 +30,7 @@ afficheQuestion(index);
 
 function afficheReponses(numeroQuestion) {
     let tabRep = Quiz[numeroQuestion].reponses
-    console.log(tabBoutons);
+    console.log(`affiche le tableau de boutons: ${tabBoutons}`);
     console.log(tabRep);
     let indice = 0;
     for (let item of tabRep){ 
@@ -66,7 +66,8 @@ function boutonAction () {
         console.log(item.id);
         item.addEventListener("click", () => {
             if (correctAnswer === item.id) {
-                console.log("vincent est un BOSSS"); 
+                score = score + 1;
+                console.log(`le score actuel est ${score}`);
             } else {
                 item.style.background = 'red'
             }

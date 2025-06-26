41c155790d8fbb88f1b5d587551f1a5987ba57e9
AquaPulco <iris.yolal@gmail.com>
2025-06-26 16:25:57 +0200

    pull de guigui

--- game.js
+++ game.js
@@ -20,101 +20,93 @@ let correctAnswer;
 let index = 0;
 let score = 0;
 
-suivant.innerText ="suivant";
+suivant.innerText = "suivant";
+suivant.disabled = true;
+
 
 
 function afficheQuestion(indice) {
     question.innerText = Quiz[indice].question;
-    console.log(indice); 
+    console.log(indice);
 }
-
 afficheQuestion(index);
-
 
 function afficheReponses(numeroQuestion) {
     let tabRep = Quiz[numeroQuestion].reponses
     console.log(`affiche le tableau de boutons: ${tabBoutons}`);
     console.log(tabRep);
     let indice = 0;
-    for (let item of tabRep){ 
+    for (let item of tabRep) {
         tabBoutons[indice].innerText = item.reponse;
         console.log(item.reponse);
-        if (item.isCorrect===true){
+        if (item.isCorrect === true) {
             correctAnswer = tabBoutons[indice].id;
         }
         indice = indice + 1;
     }
 }
-
 afficheReponses(index);
 
-function boutonSuivant (){
-suivant.addEventListener("click", () => {
-    index = index + 1;
-   
-    
-    afficheQuestion(index);
-    afficheReponses(index);
-    for (let button of tabBoutons) {
-        button.disabled = false
-        button.style.backgroundColor = "";
-        button.style.color = ""; 
-    }   
-})
+function boutonSuivant() {
+    suivant.addEventListener("click", () => {
+        index = index + 1;
+        afficheQuestion(index);
+        afficheReponses(index);
+        for (let button of tabBoutons) {
+            suivant.disabled = true;
+            button.disabled = false;
+            button.style.backgroundColor = "";
+            button.style.color = "";
+        }
+    })
 }
-
 boutonSuivant();
 
-function boutonAction () {
+function boutonAction() {
     for (let item of tabBoutons) {
         console.log(item.id);
         item.addEventListener("click", () => {
             if (correctAnswer === item.id) {
-                score = score +1
+                score = score + 1
                 console.log("Le score c'est", score)
-                console.log("vincent est un BOSSS"); 
+                console.log("vincent est un BOSSS");
             } else {
                 item.style.background = 'red'
             }
-            for (let button of tabBoutons){
+            for (let button of tabBoutons) {
                 button.disabled = true;
                 if (button.id === correctAnswer) {
                     button.style.background = 'green'
                 }
-            }
+            } suivant.disabled = false;
         })
-
     }
 }
 boutonAction();
 
-function afficherScore(){
+function afficherScore() {
     let baliseTexte = document.createElement("h2");
     baliseTexte.innerText = `votre score est ${score}`;
     nouvellePage.appendChild(baliseTexte);
     console.log(score)
 }
 
-function dernierePage () {
+function dernierePage() {
     suivant.addEventListener("click", () => {
         if (index === 4) {
             hideQuizz.style.display = "none"
-        nouvellePage.style.display = "block"
-         console.log("index  = ", index);
-         afficherScore()
+            nouvellePage.style.display = "block"
+            console.log("index  = ", index);
+            afficherScore()
         }
     })
-
 }
 dernierePage()
 
-
-
-function refresh () {
+function refresh() {
     rejouer.addEventListener("click", () => {
         location.reload()
     })
 }
 console.log(location)
-
 refresh()
